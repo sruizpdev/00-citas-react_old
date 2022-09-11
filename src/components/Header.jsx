bc3daaf9f4d5fc379145f97c92eed0cbbd112762
Sergio Ruiz <sergiotto@outlook.com>
2022-09-11 11:08:16 +0200

    header component
    Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <h1 className="font-black text-5xl text-center md:w-2/3 mx-auto">
+      Veterinary <span className="text-indigo-600">Patiens</span>
+    </h1>
+  );
+};
+
+export default Header;
