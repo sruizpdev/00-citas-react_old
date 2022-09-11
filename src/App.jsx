2b07568fd92424070b7f5aff1a715c840f8a1647
Sergio Ruiz <sergiotto@outlook.com>
2022-09-11 11:52:25 +0200

    Form component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import Header from "./components/Header";
+import Form from "./components/Form";
 
 function App() {
   return (
     <div className="container mx-auto mt-5">
       <Header />
+      <div className="mt-12 md:flex">
+        <Form />
+      </div>
     </div>
   );
 }
